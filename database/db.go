1345e6a065dd34f6c554299a56a6060a2eddcf9b
Krovaldo <alexcar1801165@gmail.com>
2025-01-15 01:30:25 +0300

    lint fix

--- database/db.go
+++ database/db.go
@@ -2,21 +2,22 @@ package database
 
 import (
 	"fmt"
+	"os"
+
 	"github.com/jmoiron/sqlx"
 	"github.com/joho/godotenv"
-	_ "github.com/lib/pq"
-	"os"
+	_ "github.com/lib/pq" // Регистрация драйвера PostgreSQL
 )
 
 func InitDB() (*sqlx.DB, error) {
 	err := godotenv.Load("./.env")
 	if err != nil {
-		return nil, fmt.Errorf("error loading .env file: %v", err)
+		return nil, fmt.Errorf("error loading .env file: %w", err)
 	}
 
 	db, err := sqlx.Connect("postgres", os.Getenv("DATABASE_URL"))
 	if err != nil {
-		return nil, fmt.Errorf("error connecting to database: %v", err)
+		return nil, fmt.Errorf("error connecting to database: %w", err)
 	}
 
 	fmt.Println("Successfully connected to the database")
--- main.go
+++ main.go
@@ -1,11 +1,4 @@
 package main
 
-import "NotesWebApp/database"
-
 func main() {
-	pgdb, err := database.InitDB()
-	if err != nil {
-		panic(err)
-	}
-	defer pgdb.Close()
 }
